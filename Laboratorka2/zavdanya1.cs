a310b11703dcd2a63181e3f65f294237716fbec6
prokhor <gerlynd.prokhor@gmail.com>
2021-09-22 16:05:40 +0300

    “first”

---
+++ Laboratorka2/zavdanya1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Laboratorka2
+{
+    class Address
+
+    {
+
+        public string index; // индекс
+
+        public string country;     // страна
+
+        public string city;     // город
+
+        public string street;     // улица
+
+        public int house;     //номер улицы
+
+        public int apartment;     // квартира
+
+        public void GetInfo()
+
+        {
+
+            Console.WriteLine($"index: {index} \ncountry: {country} \ncity: {city} \nstreet: {street} \nhouse: {house} \napartment: {apartment}");
+
+        }
+
+    }
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Address home = new Address();
+
+            home.index = "09100";
+
+            home.country = "Ukraine";
+
+            home.city = "Bila Tserkva";
+
+            home.street = "Vasilya Stusa";
+
+            home.house = 44;
+
+            home.apartment = 92;
+
+            home.GetInfo();  
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
